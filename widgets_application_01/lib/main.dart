bf9705198a7e4553bde16660fd84714274fe5f4e
oFelpxz <felipe.ppepe0212@gmail.com>
2024-05-21 16:17:30 -0300

    Column & Expanded

--- widgets_application_01/lib/main.dart
+++ widgets_application_01/lib/main.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
 
@@ -19,48 +19,38 @@ class MyApp extends StatelessWidget {
       // SCAFFOLD: é um plano branco, funciona como um esquelo que segura todas as partes do app.
       home: Scaffold( 
       // COMPONENTES DO SCAFFOLD:
-        // BACKGROUD: Define a cor do fundo, podemos definir SHADE, passando o mouse por cima.
-        backgroundColor: Colors.blue[100], 
-
-        // APP BAR ----------------------------------
-        // APPBAR: Barra superior do aplicativo.
-        appBar: AppBar(
-          // TITLE: Widget de título do APPBAR.
-          title: Text("Widget Application",
-            style: TextStyle(color: Colors.white70),
-          ),
-          centerTitle: true,
-          // ELEVATION: Tira a elevação do aplicativo.
-          elevation: 0,
-          leading: Icon(Icons.menu, 
-            color: Colors.white70,
-          ),
-          // ACTIONS: É usada pra definir uma lista de widgets. geralmente no canto superior direito, são frequentemente usados para icones de ações.
-          actions: [IconButton(onPressed: (){}, icon: Icon(Icons.logout, color: Colors.white70))],
-          backgroundColor: Colors.black87,
-        ),
-        // APP BAR ----------------------------------
 
         // BODY -------------------------------------
         // BODY: Definimos o conteúdo principal da tela.
-        body: Center(
-          // CONTAINER: Funciona igual os containers do HTML, pelo fato de ser um widget, podemos definir filhos para ele.
-          child: Container(
-            height: 300,
-            width: 300,
-            // DECORATION: Para decorar mais.
-            decoration: BoxDecoration(
-              color: Colors.deepPurple,
-              borderRadius: BorderRadius.circular(20),
-            ),  
-            // PADDING:  all, only.
-            padding: EdgeInsets.symmetric(horizontal: 25, vertical: 10), 
-            // CHILD: Podemos adcionar mais widgets filhos do container.
-            child: Icon(Icons.favorite,
-              color: Colors.red[900],
-              size: 64,
+        // Column: É um wigdet que precisa de uma children para funcionar, ele organiza tudo em coluna.
+        body: Column(
+          // MainAxis: Alinhamentos Vertical.
+          mainAxisAlignment: MainAxisAlignment.start,
+          // CrossAxis: Alinha os elementos.
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Elemento 1:
+            // Expanded: Ele basicamente vai preencher o espaço restante.
+            Expanded(
+              child: Container(
+                color: Colors.deepPurple,
+              ),
             ),
-          ),
+            // Elemento 2:
+            Expanded(
+              // Flex: Controla o tamanho dela em comparação as outras.
+              flex: 2,
+              child: Container(
+                color: Colors.deepPurple[400],
+              ),
+            ),
+            // Elemento 3:
+            Expanded(
+              child: Container(
+                color: Colors.deepPurple[200],
+              ),
+            )
+          ],
         ),
         // BODY -------------------------------------
 
